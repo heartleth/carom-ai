bbae4e2577482abd4d336a96772c3401f27cde1f
heartleth <62895113+heartleth@users.noreply.github.com>
2022-10-29 20:39:29 +0900

    Update evolution.js

--- 3cushion/evolution.js
+++ 3cushion/evolution.js
@@ -186,13 +186,15 @@ setInterval(() => {
                 randombetweenw(adam.map(e=>e.weights[2]))
             ]));
             world = Array.from({ length: mutations }, makenew);
-            world = world.concat([
+            for (let j = 0; j < 4; j++) {
+                world = world.concat([
                     new Entity(adam[0].weights),
                     new Entity(adam[0].weights),
                     new Entity(adam[1].weights),
                     new Entity(adam[2].weights),
                     new Entity(adam[3].weights)
                 ]);
+            }
             world = world.concat(childs);
             for (let n = 0; n < worldsize; n++) {
                 world[n].nth = n;
