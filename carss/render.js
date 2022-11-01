9c931ca9011e96dbf6ecc5abc62f753bfe3e91ed
heartleth <62895113+heartleth@users.noreply.github.com>
2022-11-01 16:02:47 +0900

    Update render.js

--- carss/render.js
+++ carss/render.js
@@ -63,8 +63,8 @@ function render(ctx, balls, center, n, alive, v, s, ar) {
         ctx.font = 10 * zoom + 'px serif';
         drawBall(ctx, 'white', [center[0] + balls[0][0] * zoom, center[1] - balls[0][1] * zoom], v[0], s[0], ar[0]);
         drawBall(ctx, 'red', [center[0] + balls[1][0] * zoom, center[1] - balls[1][1] * zoom], v[1], s[1], ar[1]);
-        drawBall(ctx, 'red', [center[0] + balls[2][0] * zoom, center[1] - balls[2][1] * zoom], v[2], s[2], ar[2]);
-        drawBall(ctx, 'orange', [center[0] + balls[3][0] * zoom, center[1] - balls[3][1] * zoom], v[3], s[3], ar[3]);
+        drawBall(ctx, 'orange', [center[0] + balls[2][0] * zoom, center[1] - balls[2][1] * zoom], v[2], s[2], ar[2]);
+        // drawBall(ctx, 'orange', [center[0] + balls[3][0] * zoom, center[1] - balls[3][1] * zoom], v[3], s[3], ar[3]);
 
         // ctx.strokeStyle = 'black';    
         // ctx.strokeText(n, center[0] + (balls[0][0] - 6) * zoom, center[1] - (balls[0][1]) * zoom);
